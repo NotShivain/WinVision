a4104ffbeee19978b47d51dbefeeaaa6f8ff0070
NotShivain <shivain65@gmail.com>
2025-11-05 14:23:07 +0530

    update 4.0

--- main.py
+++ main.py
@@ -25,32 +25,6 @@ seasons = pd.read_csv(r"archive/seasons.csv")
 sprint_results = pd.read_csv(r"archive/sprint_results.csv")
 status = pd.read_csv(r"archive/status.csv")
 winvision = pd.read_csv(r"winvision.csv")
-model=pickle.load(open('winvision_model.pkl','rb'))
-def prediction(driver_name, grid, circuit_loc):
-    driver = drivers.loc[drivers['Name']==driver_name, 'driverId'].iloc[0]
-    circuit = circuits.loc[circuits['location']==circuit_loc, ['circuitId']].iloc[0]
-
-    input_data = winvision[winvision['driverId'] == driver].sort_values(by='date', ascending=False).iloc[0]
-    circuit_data = circuits[circuits['location']==circuit_loc].iloc[0]
-
-    features = {
-        'driverId': input_data['driverId'],
-        'constructorId': input_data['constructorId'],
-        'grid': grid,
-        'laps': input_data['laps'],
-        'circuitId': circuit_data['circuitId'],
-        'Constructor Experience': input_data['Constructor Experience'],
-        'Driver Experience': input_data['Driver Experience'],
-        'age': input_data['age'],
-        'driver_wins': input_data['driver_wins'],
-        'constructor_wins': input_data['constructor_wins'],
-        'prev_position': input_data['prev_position'],
-        'Driver Constructor Experience': input_data['Driver Constructor Experience'],
-        'DNF Score': input_data['DNF Score']
-        
-    }
-    features = pd.DataFrame([features])
-    return model.predict(features), model.predict_proba(features)
 def loading_animation():
     st.markdown(
          f"""
